fca39b352b143ec6f481ca67dc70e5ae5e12c978
MSX <sdrinken@users.noreply.github.com>
2025-07-01 19:28:33 +0200

    Update variables.tf

--- Storage/Terraform/variables.tf
+++ Storage/Terraform/variables.tf
@@ -8,7 +8,7 @@ variable "tenant_id" {}
 variable "subscription_id" {}
 variable "storage_account_name" {
   type    = string
-  default = "mongodbbackupstestje"
+  default = "mongodbbackupstest"
 }
 variable "storage_container_name" {
   type = string
