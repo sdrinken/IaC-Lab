1c7033f50501da42050e39363e49fc66582376d4
MSX <sdrinken@users.noreply.github.com>
2025-07-01 14:56:51 +0200

    Update backup-mongo.sh

--- Storage/Backup/backup-mongo.sh
+++ Storage/Backup/backup-mongo.sh
@@ -4,13 +4,13 @@
 STORAGE_ACCOUNT="mongodbbackupstestje"
 CONTAINER_NAME="backups"
 TARGET_DIR="/tmp/mongodump"
-URI=${{ secrets.MONGO_URI }}
+URI=$MONGO_URI
 
 # Create DIR
 sudo mkdir $TARGET_DIR
 
 # Create backup
-mongodump --uri="$URI" --archive="$TARGET_DIR/dump_$(date +%Y%m%d%h).txt" --db=employees --collection=employees
+mongodump --uri='$URI' --archive="$TARGET_DIR/dump_$(date +%Y%m%d%h).txt" --db=employees --collection=employees
 
 # Authenticate via Managed Identity
 az login --identity
